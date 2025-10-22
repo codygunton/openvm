bb94d90794f81efa9d6791852daf4719fb7c1597
Cody Gunton <codygunton@gmail.com>
2025-10-22 14:59:22 -0400

    move floats example

--- elf_build_run.sh
+++ elf_build_run.sh
@@ -7,7 +7,7 @@ BBIN=${BBIN:-0}
 RUN=${RUN:-1}
 
 if [ $BTEST = "1" ]; then
-    examples/float-minimal-test/build.sh
+    examples/floats/build.sh
 fi
 
 if [ $BBIN = "1" ]; then
@@ -15,10 +15,10 @@ if [ $BBIN = "1" ]; then
 fi
 
 if [ $RUN = "1" ]; then
-    TEST_FILE=examples/float-minimal-test/build/test.elf
-    LOG_FILE=examples/float-minimal-test/elf.log
+    TEST_FILE=examples/floats/build/test.elf
+    LOG_FILE=examples/floats/elf.log
     RUST_BACKTRACE=1 RUST_LOG=trace ./target/debug/cargo-openvm openvm run \
         --exe $TEST_FILE \
-        --config examples/float-minimal-test/openvm.toml \
+        --config examples/floats/openvm.toml \
         &> "$LOG_FILE"
 fi
